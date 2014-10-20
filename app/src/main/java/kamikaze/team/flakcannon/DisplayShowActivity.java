7cf20930b44b20413122fd6284ea2bb17f306650
Society9889 <ejo1188@rit.edu>
2014-10-20 17:41:00 -0400

    Got the patterns/color working

--- app/src/main/java/kamikaze/team/flakcannon/DisplayShowActivity.java
+++ app/src/main/java/kamikaze/team/flakcannon/DisplayShowActivity.java
@@ -54,24 +54,45 @@ public class DisplayShowActivity extends Activity {
     }
 
     public void testButton(View v) {
-        Context context = getApplicationContext();
-        int duration = Toast.LENGTH_SHORT;
+  //      Context context = getApplicationContext();
+  //      int duration = Toast.LENGTH_SHORT;
 
         int size = showStuff.size();
-        String text = showStuff.get(0).color;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+//        String text = showStuff.get(0).color;
+  //      Toast toast = Toast.makeText(context, text, duration);
+    //    toast.show();
 
         for(int i = 0; i < size; i++) {
-            explosion(v);
+
+            fireWorks fw = showStuff.get(i);
+
+            explosion(v, fw.duration, fw.color);
         }
     }
 
-    public void explosion(View v) {
+    public void explosion(View v, int size, String pattern) {
     //    new ParticleSystem(this, 15, R.drawable.star_pink, 4000)
       //          .setSpeedRange(0.2f, 0.5f)
         //        .emit(findViewById(R.id.emiter_center), 8, 2000);
-        ParticleSystem ps = new ParticleSystem(this, 100, R.drawable.star_pink, 800);
+        ParticleSystem ps;
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, pattern, duration);
+        toast.show();
+
+        if(pattern.contentEquals("star white")){
+            ps = new ParticleSystem(this, 100, R.drawable.star_white, 800);
+        } else if(pattern.contentEquals("star pink")){
+            ps = new ParticleSystem(this, 100, R.drawable.star_pink, 800);
+        } else if(pattern.contentEquals("star yellow")){
+            ps = new ParticleSystem(this, 100, R.drawable.star, 800);
+        } else if(pattern.contentEquals("confeti2")){
+            ps = new ParticleSystem(this, 100, R.drawable.confeti2, 800);
+        } else {
+            ps = new ParticleSystem(this, 100, R.drawable.confeti3, 800);
+        }
+
         ps.setScaleRange(0.7f, 1.3f);
         ps.setSpeedRange(0.2f, 0.5f);
         ps.setRotationSpeedRange(90, 180);
